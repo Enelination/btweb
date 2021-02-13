bd2e383047f2924f4d8c991db3d243062f492bea
enel <enel@gmail.com>
2021-02-13 16:29:27 +0000

    about page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Home from "./components/Pages/Home";
 import PageWrapper from "./components/PageWrapper";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import About from "./components/Pages/About";
 
 function App() {
   return (
-    <PageWrapper>
-      <Home />
-    </PageWrapper>
+    <Router>
+      <PageWrapper>
+        <Route exact={true} path="/" component={Home} />
+        <Route path="/about" component={About} />
+        {/* <Route path="/contact" component={Contact} /> */}
+      </PageWrapper>
+    </Router>
   );
 }
 
